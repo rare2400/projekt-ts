3f26c27dea8533fdcb8f3532fa6a56f23f0992df
Ramona <rare2400@student.miun.com>
2025-06-10 17:02:35 +0200

    Routing för sidornas komponenter och vid tom eller okänd path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CoursesComponent } from './pages/courses/courses.component';
+import { CourseListComponent } from './pages/course-list/course-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'Hem', component: HomeComponent },
+    { path: 'Kurser', component: CoursesComponent },
+    { path: 'Ramschema', component: CourseListComponent },
+    { path: '**', redirectTo: '/Hem', pathMatch: 'full' },
+    { path: '', redirectTo: '/Hem', pathMatch: 'full' }
+];
