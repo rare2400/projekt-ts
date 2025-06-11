f3da28b00e13b909c33715cc8f33e9e76c31b09c
Ramona <rare2400@student.miun.com>
2025-06-11 16:08:31 +0200

    hämtar in puff-komponent

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
+import { PuffsComponent } from '../../partials/puffs/puffs.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ MatButtonModule, RouterLink],
+  imports: [ MatButtonModule, RouterLink, PuffsComponent ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
