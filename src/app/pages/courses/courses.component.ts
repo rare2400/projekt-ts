9fb734ebf48b40d265ca16e255f3877266a8fa26
Ramona <rare2400@student.miun.com>
2025-06-13 13:42:58 +0200

    Hämtar kursdata, skriver ut tabell och skapar funktion för filtrering och sortering

--- src/app/pages/courses/courses.component.ts
+++ src/app/pages/courses/courses.component.ts
@@ -1,11 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { CoursedataService } from '../../services/coursedata.service';
+import { Course } from '../../models/course';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatOption, MatSelect } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-courses',
-  imports: [],
+  imports: [FormsModule, CommonModule, MatInputModule, MatSelect, MatOption, MatFormFieldModule, MatTableModule, MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.css'
 })
 export class CoursesComponent {
+  /** 
+   * signals med kursdata, 
+   * för textfiltrering, ämnesfiltrering, sorteringsnyckel, stigande/fallande sorterfing 
+   * och kolumner som visas i tabellen
+   * */
+  courses = signal(<Course[]>[]);
+  filterValue = signal<string>("");
+  filterSubject = signal<string>("");
+  sortKey = signal<keyof Course>("courseCode");
+  sortAsc = signal<boolean>(true);
+  displayedColumns: string[] = ["courseCode", "courseName", "points", "subject", "syllabus", "add"];
+
+  //injection som hämtar kursdata från CoursedataService
+  courseService = inject(CoursedataService);
+
+  //filterar och sorterar lista utifrån filter-/sorteringinställningar
+  filteredCourses = computed(() => {
+    const filterValue = this.filterValue().toLowerCase();
+    const filterSubject = this.filterSubject().toLowerCase();
+    const key = this.sortKey();
+    const asc = this.sortAsc();
+
+    //filtrerar kurser baserat på kursnamn eller kurskod och filtrerar ämne efter vald select
+    const result = this.courses().filter(course =>
+      (course.courseName.toLowerCase().includes(filterValue) || course.courseCode.toLowerCase().includes(filterValue)) &&
+      (filterSubject === "" || course.subject.toLowerCase() === filterSubject)
+    )
+      .sort((a, b) => {
+        const valueA = a[key];
+        const valueB = b[key];
+
+        //jämför värdena för riktningen av sortering (asc/desc)
+        return (valueA < valueB ? -1 : valueA > valueB ? 1 : 0) * (asc ? 1 : -1);
+      })
+    console.log("Filtered courses:", result);
+    return result;
+  });
+
+  //initialiserar komponenten och hämtar kurser från skapad service
+  ngOnInit(): void {
+    this.courseService.getCourses().subscribe((courses: Course[]) =>
+      this.courses.set(courses));
+  }
+
+  //hämtar ämnen för att filtrera kurser efter ämne och filtrerar bort dubletter
+  chooseSubject = computed(() => {
+    const allSubjects = this.courses().map(course => course.subject);
+    return [...new Set(allSubjects)];
+  });
+
+  /**
+   * sorterar kurserna baserat på vald rubrik/sorteringsnyckel
+   * sorteringsriktingen ändras vid klick på samma rubrik
+   */
+  sortBy(key: keyof Course) {
+    if (this.sortKey() === key) {
+      this.sortAsc.set(!this.sortAsc());
+    } else {
+      this.sortKey.set(key);
+      this.sortAsc.set(true);
+    }
+  }
 
 }
