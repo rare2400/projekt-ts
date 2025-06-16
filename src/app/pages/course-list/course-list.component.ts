840acf64424f1459c0343352ec5811ffefdda52e
Ramona <rare2400@student.miun.com>
2025-06-16 10:33:15 +0200

    funktion för att skriva ut tillagda kurser i tabell

--- src/app/pages/course-list/course-list.component.ts
+++ src/app/pages/course-list/course-list.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
+import { RamschemaService } from '../../services/ramschema.service';
+import { Course } from '../../models/course';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-course-list',
-  imports: [],
+  imports: [CommonModule, MatTableModule, MatButtonModule, MatIconModule],
   templateUrl: './course-list.component.html',
   styleUrl: './course-list.component.css'
 })
 export class CourseListComponent {
+  displayedColumns: string[] = ["courseCode", "courseName", "points", "subject", "syllabus", "delete"];
+
+  constructor(private ramschema: RamschemaService) { }
+
+  getCourses(): Course[] {
+    return this.ramschema.getCourses();
+  }
 
 }
